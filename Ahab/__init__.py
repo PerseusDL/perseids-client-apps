a17f15e81808306917beba880f68e1cf28d247ba
Thibault Clérice <leponteineptique@gmail.com>
2015-03-19 18:42:34 +0100

    What it should look like

--- Ahab/__init__.py
+++ Ahab/__init__.py
@@ -7,10 +7,22 @@ ahab = Blueprint('capitains_ahab', __name__)
 configuration = json.load(open("Ahab/configurations/cts.json"))
 
 
-@ahab.route("/xq", methods=["GET"])
+@ahab.route("/cts/xq", methods=["GET"])
 def xq():
     try:
         r = requests.get(configuration["endpoint"], params=request.args.to_dict())
+        if r.status_code != 200:
+            abort(404)
+        return r.text
+    except:
+        abort(404)
+    abort(404)
+
+
+@ahab.route("/cts/rest/v1.0/search", methods=["GET"])
+def xq():
+    try:
+        r = requests.get(configuration["search"], params=request.args.to_dict())
         if r.status_code != 200:
             abort(404)
         return r.text
