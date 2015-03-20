d360e863619cf939117f2bd8444df35c941cc286
Thibault Clérice <leponteineptique@gmail.com>
2015-03-20 12:34:30 +0100

    Working rest implementation

--- Ahab/__init__.py
+++ Ahab/__init__.py
@@ -1,16 +1,18 @@
 import json
 from flask import Blueprint,  abort, request
 import requests
-
+import requests_cache
 
 ahab = Blueprint('capitains_ahab', __name__)
 configuration = json.load(open("Ahab/configurations/cts.json"))
+if configuration["cache"]:
+    requests_cache.install_cache("capitains-ahab", backend="sqlite", expire_after=configuration["cache.time"])
 
 
-@ahab.route("/cts/xq", methods=["GET"])
-def xq():
+def requesting(endpoint, params):
     try:
-        r = requests.get(configuration["endpoint"], params=request.args.to_dict())
+        r = requests.get(endpoint, params=params)
+        print(r)
         if r.status_code != 200:
             abort(404)
         return r.text
@@ -18,15 +20,117 @@ def xq():
         abort(404)
     abort(404)
 
-"""
-@ahab.route("/cts/rest/v1.0/search", methods=["GET"])
+
+def makeUrn(namespace, work=None):
+    if work is not None and len(work) > 0:
+        return "urn:cts:{0}:{1}".format(namespace, ".".join(work))
+    else:
+        return "urn:cts:{0}".format(namespace)
+
+
+@ahab.route("/cts/api", methods=["GET"])
 def xq():
-    try:
-        r = requests.get(configuration["search"], params=request.args.to_dict())
-        if r.status_code != 200:
-            abort(404)
-        return r.text
-    except:
-        abort(404)
-    abort(404)
+    return requesting(configuration["endpoint"], params=request.args.to_dict())
+
+
 """
+RESTful API
+"""
+if configuration["restfull"] is True:
+    @ahab.route("/cts/rest/v1.0/<inventory>", methods=["GET"])
+    def getCapabilities(inventory):
+        return requesting(configuration["endpoint"], params={"request": "GetCapabilities", "inv": inventory})
+
+    @ahab.route("/cts/rest/v1.0/<inventory>/<namespace>", methods=["GET"])
+    def getNamespaceCapabilities(inventory, namespace):
+        return requesting(configuration["endpoint"], params={
+            "request": "GetCapabilities",
+            "inv": inventory,
+            "urn": makeUrn(namespace)
+        })
+
+    @ahab.route("/cts/rest/v1.0/<inventory>/<namespace>/<textgroup>", methods=["GET"])
+    def getTextGroupCapabilities(inventory, namespace, textgroup):
+        return requesting(configuration["endpoint"], params={
+            "request": "GetCapabilities",
+            "inv": inventory,
+            "urn": makeUrn(namespace, [textgroup])
+        })
+
+    @ahab.route("/cts/rest/v1.0/<inventory>/<namespace>/<textgroup>/<work>", methods=["GET"])
+    def getWorkCapabilities(inventory, namespace, textgroup, work):
+        return requesting(
+            configuration["endpoint"],
+            params={
+                "request": "GetCapabilities",
+                "inv": inventory,
+                "urn": makeUrn(namespace, [textgroup, work])
+            }
+        )
+
+    @ahab.route("/cts/rest/v1.0/<inventory>/<namespace>/<textgroup>/<work>/<text>/validReff", methods=["GET"])
+    def getWorkValidReff(inventory, namespace, textgroup, work, text):
+        return requesting(
+            configuration["endpoint"],
+            params={
+                "request": "GetValidReff",
+                "inv": inventory,
+                "urn": "urn:cts:{0}:{1}.{2}.{3}".format(namespace, textgroup, work, text),
+                "level": request.args.get("level")
+            }
+        )
+
+    @ahab.route("/cts/rest/v1.0/<inventory>/<namespace>/<textgroup>/<work>/<text>/passage/<ref>", methods=["GET"])
+    def getWorkPassage(inventory, namespace, textgroup, work, text, ref):
+        return requesting(
+            configuration["endpoint"],
+            params={
+                "request": "GetPassage",
+                "inv": inventory,
+                "urn": "urn:cts:{0}:{1}.{2}.{3}:{4}".format(namespace, textgroup, work, text, ref),
+                "level": request.args.get("level")
+            }
+        )
+
+    @ahab.route("/cts/rest/v1.0/<inventory>/<namespace>/<textgroup>/<work>/<text>/passagePlus/<ref>", methods=["GET"])
+    def getWorkPassagePlus(inventory, namespace, textgroup, work, text, ref):
+        return requesting(
+            configuration["endpoint"],
+            params={
+                "request": "GetPassagePlus",
+                "inv": inventory,
+                "urn": "urn:cts:{0}:{1}.{2}.{3}:{4}".format(namespace, textgroup, work, text, ref),
+                "level": request.args.get("level")
+            }
+        )
+
+    @ahab.route("/cts/rest/v1.0/<inventory>/<namespace>/<textgroup>/<work>/<text>/firstPassage", methods=["GET"])
+    def getWorkFirstPassage(inventory, namespace, textgroup, work, text):
+        return requesting(
+            configuration["endpoint"],
+            params={
+                "request": "GetFirstPassage",
+                "inv": inventory,
+                "urn": "urn:cts:{0}:{1}.{2}.{3}:{4}".format(namespace, textgroup, work, text),
+                "level": request.args.get("level")
+            }
+        )
+
+if "ahab.search" in configuration:
+    @ahab.route("/ahab/rest/v1.0/search", methods=["GET"])
+    def search():
+        params = request.args.to_dict().copy()
+        params.update({"request": "Search"})
+
+        return requesting(
+            configuration["ahab.endpoint"],
+            params=params 
+        )
+
+if "ahab.permalink" in configuration:
+    @ahab.route("/ahab/rest/v1.0/permalink/<ref>", methods=["GET"])
+    def permalink(ref):
+        return requesting(
+            configuration["ahab.endpoint"],
+            params=request.args.to_dict()
+        )
--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask.ext.babel import Babel
 from flask_bower import Bower
 from app import configurator
-
+from app.cache import cache
 
 app = Flask(__name__)
 babel = Babel(app)
 bower = Bower(app)
 load = configurator.get("modules")("load")
-
+cache.init_app(app)
 
 if "capitains-ahab" in load:
     from Ahab import ahab
