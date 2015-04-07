9889d6fa7ee9a4de9824103271536630dfe4ae10
Thibault Clérice <leponteineptique@gmail.com>
2015-04-07 10:32:38 +0200

    Fixed alignment + links to assets

--- app/configurations/alignment.py
+++ app/configurations/alignment.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-alignment = 
-{
+alignment = {
     "endpoint.create" : "http://sosol.perseids.org/sosol/dmm_api/create/item/AlignmentCite/DOC_REPLACE",
     "tokenization.endpoint" : "http://services2.perseids.org/llt/segtok",
     "tokenization.xslt" : "/static/xslt/segtok_to_align.xsl",
