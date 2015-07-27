b05bf5b15f2f5acd144119fd27ef49bbbdd4d2dd
Anna <akkrohn@gmail.com>
2015-07-27 10:52:53 -0400

    Revert "Annotate"

--- app/configurations/annotation.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#!! These will need to be updated once the Perseids endpoint is set up
-annotation = {
-    "endpoint.create" : "http://sosol.perseids.org/sosol/dmm_api/create/item/AlignmentCite/DOC_REPLACE",
-    "editorUrl" : "http://sosol.perseids.org/exist/rest/db/app/align-editsentence-perseids.xhtml?doc=REPLACE_DOC&s=1",
-    "collection" : "urn:cite:perseus:annotate",
-    "altVersionUrl" : "http://sosol.perseids.org/exist/rest/db/app/align-entersentence-perseids.xhtml",
-    "workListUrl" : "http://sosol.perseids.org/exist/rest/db/xq/ctsworklist.xq?inv=annotsrc"
-}
--- app/static/js/alpheios/save_data.success.js
+++
@@ -1,3 +0,0 @@
-
-	$(this).scrollTop(0);
-	$('form').find("input[type=text], textarea").val("");
