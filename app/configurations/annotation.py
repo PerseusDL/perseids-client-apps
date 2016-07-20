c2ef8e4d39584b9d3a1d25f918a40340896398c1
annakrohn <akkrohn@gmail.com>
2016-07-20 17:54:10 -0400

    commenting out test endpoint, to be removed before merge

--- app/configurations/annotation.py
+++ app/configurations/annotation.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #!! These will need to be updated once the Perseids endpoint is set up
 annotation = {
-    "endpoint.create" : "http://127.0.0.1:5050/save_data",
-    #"endpoint.create" : "http://digmill.perseus.org/digmil/save_data",
+		#!! For testing, remove before final merge !!
+    #"endpoint.create" : "http://127.0.0.1:5050/save_data",
+    "endpoint.create" : "http://digmill.perseus.org/digmil/save_data",
     "editorUrl" : "http://sosol.perseids.org/exist/rest/db/app/align-editsentence-perseids.xhtml?doc=REPLACE_DOC&s=1",
     "collection" : "urn:cite:perseus:annotate",
     "workListUrl" : "http://cts.perseids.org"
