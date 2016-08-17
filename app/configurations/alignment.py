481f07f66f2e463206c14a9345f37259e894286b
Bridget Almas <balmas@gmail.com>
2016-08-17 08:23:15 -0400

    fixing path to alpheios editor on sosol

--- app/configurations/alignment.py
+++ app/configurations/alignment.py
@@ -4,7 +4,7 @@ alignment = {
     "endpoint.create" : "http://sosol.perseids.org/sosol/dmm_api/create/item/AlignmentCite/DOC_REPLACE",
     "tokenization.endpoint" : "http://services2.perseids.org/llt/segtok",
     "tokenization.xslt" : "/apps/static/xslt/segtok_to_align.xsl",
-    "editorUrl" : "http://sosol.perseids.org/exist/rest/db/app/align-editsentence-perseids.xhtml?doc=REPLACE_DOC&s=1",
+    "editorUrl" : "http://sosol.perseids.org/alpheios/app/align-editsentence-perseids.xhtml?doc=REPLACE_DOC&s=1",
     "collection" : "urn:cite:perseus:align",
     "workListUrl" : "http://cts.perseids.org",
 }
